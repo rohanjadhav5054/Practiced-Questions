35d4946e3249d51e62fc4ff73aeccd2725d39d8b
Rohan <xrohanjadhav5054@gmail.com>
2025-11-28 12:06:53 +0530

    Simple Java Structure

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String args[]){
+
+    }
+}
+
+//public - access modifier - so we can access that class from anywhere 
+//main - is the starting point of the program 
+//
